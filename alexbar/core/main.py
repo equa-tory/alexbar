3a6dea60e2e49ff6e87c6752fd721388e1c52a21
Equa <TheSuspect9980@gmail.com>
2024-08-26 14:45:47 +0300

    17.2 converter translation

--- alexbar/core/main.py
+++ alexbar/core/main.py
@@ -7,43 +7,65 @@ OUTPUT = "./templates/core/posts/tmp.html"
 
 PATTERNS = [
 (r'h:(.*?);', lambda content: f"""
-<div class="header" style="height: fit-content; ">
+<div class="header" style="height: fit-content;">
     <img src="{{% static 'core/img/posts/{content}' %}}" style="width: 100%; border-radius: 6px;">
 </div>"""),
+
 
 (r'in:', lambda content: f"""
 <div class="in-post">"""),
 (r':in;', lambda content: f"""</div>"""),
 
+
 (r'vert:', lambda content: f"""
     <div class="vert10">"""),
 (r':vert;', lambda content: f"""    </div>"""),
 
-(r't:(.*?);', lambda content: f"""        <div class="h1">
+
+(r'en-t:(.*?);', lambda content: f"""        <div class="h1" id="en">
+            <h1 class="h1">{content}</h1>
+        </div>"""),
+(r'ru-t:(.*?);', lambda content: f"""        <div class="h1" id="ru">
             <h1 class="h1">{content}</h1>
         </div>"""),
 
-(r'd:(.*?);', lambda content: f"""        <div class="r3">
+
+(r'en-d:(.*?);', lambda content: f"""        <div class="r3" id="en">
             <h3>{content}</h3>
         </div>"""),
+(r'ru-d:(.*?);', lambda content: f"""        <div class="r3" id="ru">
+            <h3>{content}</h3>
+        </div>"""),
+
 
 (r'sep;', lambda content: f"""
     <div class="separator long-separator"></div>
 """),
 
-(r's:(.*?);', lambda content: f"""    <div class="span"><span>{content}</span></div>"""),
 
-(r'p:(.*?);', lambda content: f"""    <div class="p">
+(r'en-s:(.*?);', lambda content: f"""    <div class="span" id="en"><span>{content}</span></div>"""),
+(r'ru-s:(.*?);', lambda content: f"""    <div class="span" id="ru"><span>{content}</span></div>"""),
+
+
+(r'en-p:(.*?);', lambda content: f"""    <div class="p" id="en">
+        <p>
+            {content}
+        </p>
+    </div>"""),
+(r'ru-p:(.*?);', lambda content: f"""    <div class="p" id="ru">
         <p>
             {content}
         </p>
     </div>"""),
 
+
 (r'hor:', lambda content: f"""    <div class="hor">"""),
 (r':hor;', lambda content: f"""    </div>"""),
 
+
 (r'cen-title:', lambda content: f"""    <div class="cen">"""),
 (r':cen-title;', lambda content: f"""    </div>"""),
+
 
 (r'img-set:', lambda content: f"""<div class="cen">
     <div class="img6">"""),
@@ -51,15 +73,20 @@ PATTERNS = [
 (r':img-set;', lambda content: f"""    </div>
 </div>"""),
 
-(r'n:(.*?);', lambda content: f"""        <h2>{content}</h2>"""),
+
+(r'en-n:(.*?);', lambda content: f"""        <h2 id="en">{content}</h2>"""),
+(r'ru-n:(.*?);', lambda content: f"""        <h2 id="ru">{content}</h2>"""),
+
 
 (r'a:(.*?);', lambda content: f"""        <audio controls>
             <source src="{{% static 'core/audio/{content}' %}}">
         </audio>"""),
 
+
 (r'sp;', lambda content: f"""
         <div class="space"></div>
 """),
+
 
 (r'gif:(.*?);', lambda content: f"""
 <div class="in-post">
