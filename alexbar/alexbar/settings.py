344ed5bdba3b19251487b8e009bd90b17b227b15
Equa <TheSuspect9980@gmail.com>
2024-09-27 20:08:52 +0300

    20.1 hfix

--- alexbar/alexbar/settings.py
+++ alexbar/alexbar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vt0$3%p+e9v4923b(78o@_@2f9)293gy&sp-85a-o28um8tx9m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['80.251.226.62', '147.45.136.188', '62.113.119.238', '127.0.0.1', 'localhost']
